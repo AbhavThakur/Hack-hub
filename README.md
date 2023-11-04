3fe84e92bb59bee87997e87b0646f85c3cf696cb
Abhav Thakur <abhav.thakur>
2023-11-05 02:11:11 +0530

    add react router package and setup the routes for sign In ,sign Up and home page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# React + TypeScript + Vite
+# Hack-hub
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A social media platform for sharing your memory with Developers.
 
 Currently, two official plugins are available:
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import AuthLayout from './auth/AuthLayout';
+import SigninForm from './auth/forms/SigninForm';
+import SignupForms from './auth/forms/SignupForms';
 import './global.css';
+import RootLayout from './root/RootLayout';
+import { Home } from './root/pages';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForms />} />
+          <Route path="*" element={<div>404</div>} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
