8b7e9cc4c1079d804cea03487219d0581005f58d
Abhav Thakur <abhav.thakur>
2023-11-06 21:07:52 +0530

    Added Appwrite for storing data on cloud,creating users and react-hook-form for validation of forms

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -9,10 +9,14 @@ function AuthLayout() {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
-          <img src="/public/assets/images/side-img.svg" alt="OnBoardingImage" />
+          <img
+            src="/assets/images/side-img.svg"
+            alt="OnBoardingImage"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,12 @@
+function Loader() {
+  return (
+    <img
+      src="/public/assets/icons/loader.svg"
+      alt="loader"
+      width={24}
+      height={24}
+    />
+  );
+}
+
+export default Loader;
